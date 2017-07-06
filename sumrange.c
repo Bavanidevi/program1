f1779d6fd414a0c9cce923e938aa8080db078a42
Bavanidevi <bavab.tech@gmail.com>
2017-07-06 10:39:04 +0530

    Create sumrange.c

---
+++ sumrange.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int sum=0,n1,n2;
+printf("Enter the range\n");
+scanf(%d",&n1,&n2);
+while(n1<=n2)
+{
+sum=sum+n1;
+n1++;
+}
+printf("THe sum between the range is %d");
+getch();
+}
+
+
