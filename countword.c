b4f57cb5eb455c9a335eb8ffba940c8b1531216e
Bavanidevi <bavab.tech@gmail.com>
2017-07-13 09:17:10 +0530

    Create countword.c

---
+++ countword.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int countword=0;
+char str[50];
+printf("Enter the string\n");
+gets(str);
+for(i=0;i<str[i]=" ";i++)
+{
+countword++;
+}
+printf("The number of characters are %d",countword+1);
+getch();
+}
