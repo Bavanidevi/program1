b165c1dac9475f81406049a7d5fecf86610825af
Bavanidevi <bavab.tech@gmail.com>
2017-07-17 15:50:32 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a1,b1,c1;
+printf("Enter the three numbers\n");
+scanf("%d %d %d",&a,&b,&c);
+if(a1>b1&&a1>c1)
+{
+printf("The number %d is greater",a1);
+}
+elseif(b1>c1)
+{
+printf("The number %d is greater",b1);
+}
+else
+{
+printf("The number %c is greater",c1);
+}
+getch();
+}
