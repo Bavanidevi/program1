04e85218b54cfe62c3494239be69ec46c0109f37
Bavanidevi <bavab.tech@gmail.com>
2017-07-27 16:05:16 +0530

    Create prompt.c

---
+++ prompt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n1;
+printf("Enter the number between 0 to 100\n");
+scanf("%d",&n1);
+if(n1>=100)
+{
+printf("Error\n");
+printf("Enter the valid input\n");
+}
+getch();
+}
